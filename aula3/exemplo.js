28452f93a44a4dd0faf22b7d808557fed3294039
Caroline P. da Silva <carolinepedasil@gmail.com>
2021-11-10 20:43:39 -0300

    Exercicios - Parte 1

---
+++ aula3/exemplo.js
@@ -0,0 +1,11 @@
+const nome = "Caroline";
+const idade = 24;
+const ehMaiorDeIdade = true;
+
+console.log(nome);
+console.log(idade);
+console.log(ehMaiorDeIdade);
+
+console.log(nome, idade, ehMaiorDeIdade);
+console.log(nome + idade + ehMaiorDeIdade);
+console.log("Meu nome é " + nome + " e eu tenho " + idade);
