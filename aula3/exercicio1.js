401758a9b65ef540c35a9cfb0e9edcc02b39cb37
Caroline P. da Silva <carolinepedasil@gmail.com>
2021-11-10 23:03:07 -0300

    Exercícios - Aula 3

---
+++ aula3/exercicio1.js
@@ -0,0 +1,11 @@
+// Crie 4 variáveis para armazenar os seguintes dados referentes a um carro: 
+
+var modelo = "Ferrari";
+var cor = "Vermelha";
+var ano = 2020;
+var novo = true; // mas pode ser false também
+
+console.log(modelo);
+console.log(cor);
+console.log(ano);
+console.log("Meu carro é novo? ", novo);
---
+++ aula3/exercicio2.js
@@ -0,0 +1,18 @@
+/* 
+    Vamos explorar a criação de variáveis sem inicializa-las com um valor! 
+    Coloque como comentário no código o que acontece quando você executa os seguintes trechos de código:
+*/
+
+var meuNome;
+console.log(meuNome);
+// Apareceu: undefined
+
+/*
+    Agora altere a variável e atribua a ela o seu nome, exemplo: 
+*/
+
+var meuNome;
+meuNome = "Caroline"
+console.log(meuNome)
+// agora no terminal apareceu: Caroline
+
